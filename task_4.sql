c06bd1f3edb9b1aa8b63553f2da6e426e556d54f
kesavan-t-dev <kesavan.t@mitrahsoft.in>
2025-12-31 16:41:49 +0530

    query_1 updated

--- task_4.sql
+++ task_4.sql
@@ -1,1 +1,22 @@
 
+--USE DATABASE
+use kesavan_db
+GO
+
+
+-- Query 1:  Retrieve tasks along with their project details
+SELECT 
+    t.task_id,
+    t.task_name,
+    t.prioritys,
+    p.project_id,
+    p.project_name,
+    p.starts_date,
+    p.end_date,
+    t.statuss
+FROM 
+    task t
+INNER JOIN 
+    project p
+ON 
+    t.project_id = p.project_id;
